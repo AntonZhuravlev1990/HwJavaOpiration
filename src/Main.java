b4f3536b4690c3fceeeaba5f728621e5b97590a9
Neto study <antonzhuravlev831@gmail.com>
2025-06-06 21:07:16 +0800

    first commit

--- src/Main.java
+++ src/Main.java
@@ -5,7 +5,9 @@ public class Main {
         int ticket = 16500; // стоимость билета
         int bonus = 20; // потраченые рубли за 1 милю
 
+        System.out.println((ticket) + " Стоимость билета");
+        System.out.println((bonus) + " потраченые рубли за 1 милю");
         System.out.println("Расчет бонусных милей за купленый билет");
-        System.out.println((ticket / bonus) + "мили");
+        System.out.println((ticket / bonus) + " мили");
     }
 }
